3d28e35b92dfa96c69f34fddf6afd4fe8bea81d9
james.yum <james.yum@7d0b6272-96af-11dd-92de-c1065bedd0ba>
2008-10-12 15:13:26 +0000

    initial import.

---
+++ main.js
@@ -0,0 +1,32 @@
+﻿// Copyright 2008 Google Inc.  All Rights Reserved.
+
+/**
+ * @fileoverview Functions for the main view of the gadget.
+ */
+
+function view_onOpen() {
+  // Set global variables if not defined by the API. These were introduced in
+  // 5.8 and might not be in other platforms yet.
+  if (gddDetailsViewFlagNoFrame == undefined) {
+    gddDetailsViewFlagNoFrame = 0;
+  }
+  if (gddDetailsViewFlagDisableAutoClose == undefined) {
+    gddDetailsViewFlagDisableAutoClose = 0;
+  }
+
+  options.encryptValue(OPTIONS.MAIL);
+  options.encryptValue(OPTIONS.PASSWORD);
+  options.encryptValue(OPTIONS.AUTH);
+
+  options.putDefaultValue(OPTIONS.MAIL, '');
+  options.putDefaultValue(OPTIONS.PASSWORD, '');
+  options.putDefaultValue(OPTIONS.REMEMBER, true);
+  options.putDefaultValue(OPTIONS.HOUR24, false);
+  options.putDefaultValue(OPTIONS.VIEW, OPTIONS.CALENDARVIEW);
+  options.putDefaultValue(OPTIONS.WEEKSTART, START_SUNDAY);
+  options.putDefaultValue(OPTIONS.AUTH, '');
+  options.putDefaultValue(OPTIONS.USE_QUICK_ADD, false);
+
+  g_calendarGadget = new CalendarGadget();
+  g_calendarGadget.run();
+}
---
+++ quickadd.js
@@ -0,0 +1,59 @@
+﻿// Copyright 2008 Google Inc.  All Rights Reserved.
+
+/**
+ * @fileoverview Quick add event dialog.
+ */
+
+var g_quickAdd = null;
+
+/**
+ * onOpen event handler for quick add input details view.
+ */
+function view_onopen() {
+  g_quickAdd = new QuickAddDialog();
+  setTimeout(quickAddEdit.focus(), 100);
+}
+
+/**
+ * Class for interaction with the quick add dialog.
+ * @constructor
+ */
+function QuickAddDialog() {
+  quickAddEdit.onkeypress = Utils.bind(this.onKeyPress, this);
+  btnAdd.onclick = Utils.bind(this.onSubmit, this);
+  imgClose.onclick = Utils.bind(this.close, this);
+}
+
+/**
+ * OnkeyPress handler for the quick add input edit.
+ */
+QuickAddDialog.prototype.onKeyPress = function() {
+  if (event.keyCode == 13) {
+    this.createEvent(quickAddEdit.value);
+  }
+};
+
+/**
+ * onSubmit handler when the button for adding the event is clicked.
+ */
+QuickAddDialog.prototype.onSubmit = function() {
+  this.createEvent(quickAddEdit.value);
+};
+
+/**
+ * Trigger options so that new event will be created in main view.
+ * @param {string} text Quick add text.
+ */
+QuickAddDialog.prototype.createEvent = function(text) {
+  if (text.length > 0) {
+    options.putValue(OPTIONS.QUICKADD, text);
+    this.close();
+  }
+};
+
+/**
+ * Close handler when the X in the top-right corner is clicked.
+ */
+QuickAddDialog.prototype.close = function() {
+  options.putValue(OPTIONS.CLOSE_DETAILS, true);
+};
