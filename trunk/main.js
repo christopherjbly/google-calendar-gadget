d57e2eb059d6cf692835298a8561b0a5c80ba6c2
james.yum <james.yum@7d0b6272-96af-11dd-92de-c1065bedd0ba>
2009-01-09 01:42:28 +0000

    ensure OPTIONS.CLOSE_DETAILS is false at startup so an onchange fires on first attempt to close details view. see optionchanged handler in ui_main.js.

--- trunk/main.js
+++ trunk/main.js
@@ -39,6 +39,7 @@ function view_onOpen() {
   options.putDefaultValue(OPTIONS.AUTH, '');
   options.putDefaultValue(OPTIONS.USE_QUICK_ADD, false);
   options.putDefaultValue(OPTIONS.UPGRADE, strings.GADGET_VERSION);
+  options.putValue(OPTIONS.CLOSE_DETAILS, false);
 
   g_calendarGadget = new CalendarGadget();
   g_calendarGadget.run();
